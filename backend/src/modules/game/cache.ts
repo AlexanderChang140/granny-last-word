87cb0b794d8f8e4f121784fb950f646145ce51be
Alex <alexanderchang140@gmail.com>
2026-03-17 23:37:56 -0700

    feat: implement server-side cache

---
+++ backend/src/modules/game/cache.ts
@@ -0,0 +1,32 @@
+import { Mutex, } from "async-mutex";
+import type { SessionData } from "./types.js";
+
+const cache: Map<string, SessionData> = new Map();
+const locks = new Map<string, Mutex>();
+
+function getLock(sessionId: string): Mutex {
+    const lock = locks.get(sessionId) ?? new Mutex();
+    locks.set(sessionId, lock);
+    return lock;
+}
+
+export async function get(sessionId: string) {
+    return cache.get(sessionId);
+}
+
+export async function set(sessionId: string, data: SessionData) {
+    const mutex = getLock(sessionId);
+    
+    await mutex.runExclusive(() => {
+        cache.set(sessionId, data);
+    });
+}
+
+export async function remove(sessionId: string): Promise<void> {
+    const mutex = getLock(sessionId);
+
+    await mutex.runExclusive(() => {
+        cache.delete(sessionId);
+        locks.delete(sessionId);
+    });
+}
