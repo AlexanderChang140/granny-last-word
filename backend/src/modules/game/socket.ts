11804f182309cd00c4be57f6b4cc2afcb110c51b
Alex <alexanderchang140@gmail.com>
2026-03-22 00:32:44 -0700

    fix: set session at start of connection style: use single quotes

--- backend/src/modules/game/socket.ts
+++ backend/src/modules/game/socket.ts
@@ -1,18 +1,20 @@
-import type { Socket } from "socket.io";
-import { disconnect, startBattle, submitWord } from "./service.js";
+import type { Socket } from 'socket.io';
+import { disconnect, startBattle, submitWord } from './service.js';
+import { set } from './cache.js';
 
 export function connect(socket: Socket) {
     console.log(`User connected: ${socket.id}`);
+    set(socket.id, {});
 
-    socket.on("start_battle", async () => {
+    socket.on('start_battle', async () => {
         startBattle(socket);
     });
 
-    socket.on("submit_word", async (payload) => {
+    socket.on('submit_word', async (payload) => {
         submitWord(socket, payload);
     });
 
-    socket.on("disconnect", () => {
+    socket.on('disconnect', () => {
         disconnect(socket);
     });
 }
